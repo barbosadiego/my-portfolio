51a10c0b73a209f9e8e597d862cb83960e9b4bf0
diegobarbosa <diego.gutemberg@hotmail.com>
2022-09-12 16:12:17 -0300

    add switch style and updade buttons selectors

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 const items = document.querySelectorAll("[data-content] > div");
 const buttons = document.querySelectorAll("[data-buttons] > button");
+const switchButton = document.querySelector("[data-switch]");
 
 items[0].classList.add('active');
 buttons[0].classList.add('active');
 
 function aciveTab(index){
+  switchButton.classList.toggle('active');
+  
   items.forEach(item => item.classList.remove('active'));
   items[index].classList.add('active');
 
