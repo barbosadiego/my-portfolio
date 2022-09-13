2b927cbd81da3eacda07cb93baa5b40245dbc798
diegobarbosa <diego.gutemberg@hotmail.com>
2022-09-13 11:09:52 -0300

    add js for dark mode

--- js/main.js
+++ js/main.js
@@ -23,9 +23,28 @@ if (items.length && buttons.length && switchButton) {
 
 // switch dark-mode
 const btn = document.querySelector('[data-darkmode]');
+const bodyEl = document.querySelector('body')
 
 if (btn) {
-  btn.addEventListener('click', () =>
-    document.querySelector('body').classList.toggle('dark'),
-  );
+  btn.addEventListener('click', () => {
+    localStorage.darkmode = true;
+    bodyEl.classList.toggle('dark');
+    handleDark();
+  });
 }
+
+function handleDark(){
+  const state = bodyEl.classList.contains('dark')
+  localStorage.darkmode = state
+}
+
+function checkDark() {
+  const mode = JSON.parse(localStorage.darkmode);
+  if (mode) {
+    bodyEl.classList.add('dark');
+  } else {
+    bodyEl.classList.remove('dark');
+  }
+}
+
+checkDark();
